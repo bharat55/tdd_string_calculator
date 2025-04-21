c13ffde9f723d0a337e54028774861ea602b49dc
Bharat Rathor <bharat.rathor@gemsessence.com>
2025-04-21 16:15:52 +0530

    Single digit string input spec:- Tried fixing single string number input spec

--- app/lib/calculator/string_calculator.rb
+++ app/lib/calculator/string_calculator.rb
@@ -2,6 +2,8 @@ module Calculator
   class StringCalculator
     def self.add(numbers)
       return 0 if numbers.empty?
+      numbers = numbers.split
+      return numbers.first if numbers.length == 1
     end
   end
 end
