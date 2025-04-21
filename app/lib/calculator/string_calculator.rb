55c01d4655af7355add656bdbcf3961d7eabcdb6
Bharat Rathor <bharat.rathor@gemsessence.com>
2025-04-21 16:19:22 +0530

    Single digit string input spec:- Fix spec (datatype mismatch failure)
    https://github.com/bharat55/tdd_string_calculator/issues/6

--- app/lib/calculator/string_calculator.rb
+++ app/lib/calculator/string_calculator.rb
@@ -2,7 +2,7 @@ module Calculator
   class StringCalculator
     def self.add(numbers)
       return 0 if numbers.empty?
-      numbers = numbers.split
+      numbers = numbers.split.map(&:to_i)
       return numbers.first if numbers.length == 1
     end
   end
