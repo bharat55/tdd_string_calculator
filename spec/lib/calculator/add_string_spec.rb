ab4e429f28b036188c4b7dd2835f6cac7d4b00eb
Bharat Rathor <bharat.rathor@gemsessence.com>
2025-04-21 16:13:12 +0530

    Write a spec for single string number input

--- spec/lib/calculator/add_string_spec.rb
+++ spec/lib/calculator/add_string_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe Calculator::StringCalculator do
         expect(result).to eq(0)
       end
     end
+
+    context 'when the input is a non-empty string' do
+      it 'returns the integer value of the string when it contains a single number' do
+        expect(described_class.add("1")).to eq(1)
+      end
+    end
+  
   end
 end
