32ddcdb8850e7e5c831be712599054c4f02bb652
Bharat Rathor <bharat.rathor@gemsessence.com>
2025-04-21 16:43:19 +0530

    2.2 Fix two digit number sum integration
    https://github.com/bharat55/tdd_string_calculator/issues/7

--- app/lib/calculator/string_calculator.rb
+++ app/lib/calculator/string_calculator.rb
@@ -2,7 +2,7 @@ module Calculator
   class StringCalculator
     def self.add(numbers)
       return 0 if numbers.empty?
-      numbers = numbers.split.map(&:to_i)
+      numbers = numbers.split(",").map(&:to_i)
       return numbers.first if numbers.length == 1
       return numbers.sum
     end
