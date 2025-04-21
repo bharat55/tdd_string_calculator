b8de562a7d951d3cc4fbf553ed275385d016bb8c
Bharat Rathor <bharat.rathor@gemsessence.com>
2025-04-21 17:17:38 +0530

    5. development:- returns the sum when a custom delimiter is provided

--- app/lib/calculator/string_calculator.rb
+++ app/lib/calculator/string_calculator.rb
@@ -2,10 +2,15 @@ module Calculator
   class StringCalculator
     def self.add(numbers)
       return 0 if numbers.empty?
-      numbers = numbers.gsub("\n", ",")
-      numbers = numbers.split(",").map(&:to_i)
-      return numbers.first if numbers.length == 1
-      return numbers.sum
+
+      delimiter = ","
+      if numbers.start_with?("//")
+        delimiter_section, numbers = numbers.split("\n", 2)
+        delimiter = delimiter_section[2] 
+      end
+
+      numbers = numbers.gsub("\n", delimiter)
+      numbers.split(delimiter).map(&:to_i).sum
     end
   end
 end
